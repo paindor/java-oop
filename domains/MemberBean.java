b1f837c8824b2b09d17276ba41c4a95c070ba36e
최광진 <53929412+paindor@users.noreply.github.com>
2019-08-23 00:54:04 +0900

    Add files via upload

--- domains/MemberBean.java
+++ domains/MemberBean.java
@@ -1,7 +1,8 @@
 package com.bitcamp.domains;
 
 public class MemberBean {
-	private String  id, name, pw, blood, ssn;
+
+	private String id, pw, name, ssn, blood;
 	private double height, weight;
 	public String getId() {
 		return id;
@@ -9,52 +10,53 @@ public class MemberBean {
 	public void setId(String id) {
 		this.id = id;
 	}
-	public String getName() {
-		return this.name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
 	public String getPw() {
-		return this.pw;
+		return pw;
 	}
 	public void setPw(String pw) {
 		this.pw = pw;
 	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSsn() {
+		return ssn;
+	}
+	public void setSsn(String ssn) {
+		this.ssn = ssn;
+	}
 	public String getBlood() {
-		return this.blood;
+		return blood;
 	}
 	public void setBlood(String blood) {
 		this.blood = blood;
 	}
 	public double getHeight() {
-		return this.height;
+		return height;
 	}
 	public void setHeight(double height) {
 		this.height = height;
 	}
 	public double getWeight() {
-		return this.weight;
+		return weight;
 	}
 	public void setWeight(double weight) {
 		this.weight = weight;
 	}
-	public void setSsn(String ssn) {
-		this.ssn = ssn;
-	}
-	public String getSsn() {
-		return this.ssn;
-	}
+	
 	public String tosString() {
-		return String.format("[회원정보\n"
-				+ "아이디 : %s \n"
-				+ "이름: %s\n"
-				+ "비밀번호: %s\n"
-				+ "혈액형: %s\n"
-				+ "주민번호: %s\n"
-				+ "키: %s\n"
-				+ "몸무게: %s] \n",
-				id, name, pw, blood, ssn, height, weight);
+		return String.format("-회원정보-\n"
+				+ "아이디:%s\n"
+				+ "비밀번호:%s\n"
+				+ "이름:%s\n"
+				+ "주민번호:%s\n"
+				+ "혈액형:%s\n"
+				+ "키%s\n"
+				+ "몸무게%s\n"
+				,id, pw, name, ssn, blood, height, weight);
 	}
-
+	
 }
